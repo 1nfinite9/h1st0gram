64afcc14882584c14514e8b49a94e8764d57e0fb
OpenShift guest <road2rails@gmail.com>
2014-01-23 04:21:47 -0500

    ==First Running Program== 1. Changed algorithms to fill_* type functions    by filling x_axis and y_axis    Then calling fill_column through fill_table    Current bugs: fill_table seems to affect the x_axis display

--- h1st0gram.h
+++ h1st0gram.h
@@ -1,6 +1,11 @@
-#define X_SIZE 20
+#define X_SIZE 10
 #define Y_SIZE 20
 
-void print_h1s(int *pDim);
+void fill_x_axis(int axis[], int n);
+void fill_y_axis(int axis[], int n);
 
-void print_h1s_singleLine();
+void fill_column(char table[][X_SIZE*2+1], int col, int data[]);
+void fill_table(char table[][X_SIZE*2+1], int col, int data[]);
+
+void print_x_axis(int axis[], int n);
+void print_h1s_singleLine(int y_axis[], char table[][X_SIZE*2+1]);
--- main.c
+++ main.c
@@ -6,15 +6,21 @@ int main() {
     // Initialize test data
     int data[10] = { 1, 0, 3, 5, 10, 15, 9, 6, 0, 1 };
     int *pData = data;
+    int y_axis[Y_SIZE+1];
+    int x_axis[X_SIZE];
+    char table[Y_SIZE][X_SIZE*2+1] = {{' '}};
     
     // Receive input from user or open file
+    fill_y_axis(y_axis, Y_SIZE);
+    fill_x_axis(x_axis, X_SIZE);
     
+    fill_table(table, X_SIZE, data);
     
     // Handle data and processing
     
-    
     // Output to screen
-    print_h1s_singleLine();
+    print_h1s_singleLine(y_axis, table);
+    print_x_axis(x_axis, X_SIZE);
     
     return 0;
 }
